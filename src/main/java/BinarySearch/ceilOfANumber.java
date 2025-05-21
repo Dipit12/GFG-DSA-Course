889986aadd1401c2aa94dc8021db7215e7b7d624
Dipit12 <dipitmadan@gmail.com>
2025-05-22 01:54:27 +0530

    binary search implemenration completed

---
+++ src/main/java/BinarySearch/ceilOfANumber.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class ceilOfANumber {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,78,234};
+        System.out.println(binarySearch(arr,2342));
+    }
+    // return -1 if the target element isnt found
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int middle  = start + (end - start)/2;
+
+            if(target > arr[middle]){
+                start = middle + 1;
+            }else if(target < arr[middle]){
+                end = middle - 1;
+            }else{
+                // ans found
+                return middle;
+            }
+
+        }
+        return start;
+    }
+}
